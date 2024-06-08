9481c059fc87ab30013678fdaefb77c6c0d09566
shinhojung <shinhojung814@gmail.com>
2024-06-08 23:50:30 +0900

    Removed miscellaneous

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-
 import classNames from 'classnames/bind'
 import styles from './App.module.scss'
 
@@ -10,20 +6,7 @@ const cx = classNames.bind(styles)
 function App() {
   return (
     <div className={cx('container')}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   )
 }
