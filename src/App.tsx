12c9fbccac80ddfee88e6b6f14203c4d6f692fa2
shinhojung <shinhojung814@gmail.com>
2024-06-10 14:35:25 +0900

    Changed craco.config.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import classNames from 'classnames/bind'
+import { useEffect } from 'react'
 import styles from './App.module.scss'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  return (
-    <div className={cx('container')}>
-      App
-    </div>
-  )
+  // 1. wedding 데이터 호출
+  useEffect(() => {
+    fetch('http://localhost:8888/wedding')
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        console.log('data', data)
+      })
+  }, [])
+
+  return <div className={cx('container')}>App</div>
 }
 
 export default App
